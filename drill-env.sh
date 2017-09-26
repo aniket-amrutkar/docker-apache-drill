dde1d09eace5b6cc96cf2c024bed6bb282c188bb
Lenin <lsubramanian@maverixbio.com>
2017-09-26 11:46:12 -0700

    increasing user server timeout 300

--- drill-env.sh
+++ drill-env.sh
@@ -15,5 +15,5 @@ export DRILL_JAVA_OPTS="-Xms$DRILL_HEAP -Xmx$DRILL_HEAP -XX:MaxDirectMemorySize=
 export SERVER_GC_OPTS="-XX:+CMSClassUnloadingEnabled -XX:+UseG1GC "
 
 
-echo "drill.exec:{ cluster-id: \"$DRILL_CLUSTER\", zk.connect: \"$ZOOKEEPER\", buffer.size : \"$DRILL_EXEC_BUFFER_SIZE\" }" > /opt/drill/apache-drill-${DRILL_VERSION}/conf/drill-override.conf
+echo "drill.exec:{ cluster-id: \"$DRILL_CLUSTER\", zk.connect: \"$ZOOKEEPER\", buffer.size : \"$DRILL_EXEC_BUFFER_SIZE\", rpc.user.timeout: 3600 }" > /opt/drill/apache-drill-${DRILL_VERSION}/conf/drill-override.conf
 
