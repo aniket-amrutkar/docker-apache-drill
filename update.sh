43dfe4033aa34cb10277f9bb003caf7a22d27512
Phagun Baya <phagun.baya@gmail.com>
2017-09-30 19:17:25 -0700

    fixing the update script

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ counter=1
 while [ $counter -le 10 ]
 do
 	response=$(curl --write-out %{http_code} --silent --output /dev/null -H "Content-Type: application/json" -X POST  --data "@/dfs.config" http://localhost:8047/storage/dfs.json)
-  if [ response -eq 200 ]
+  if [ $response -eq 200 ]
   then
      break
   fi
